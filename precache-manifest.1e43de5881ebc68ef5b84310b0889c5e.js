b870644207976d3b136e9ac4156de7ebda700045
Jose Acevedo <jdesignermayor@gmail.com>
2020-07-18 22:23:01 -0500

    Updates

---
+++ precache-manifest.1e43de5881ebc68ef5b84310b0889c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b6f2514b4b32001d4fd8c363428e20",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "910b810b290d3753d2c9",
+    "url": "/monsters-rolodex/static/css/main.c62308c4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910b810b290d3753d2c9",
+    "url": "/monsters-rolodex/static/js/main.c866687e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
