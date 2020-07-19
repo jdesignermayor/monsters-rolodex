cb73f88f169f24737c24f023e46633a9f4562adc
Jose Acevedo <jdesignermayor@gmail.com>
2020-07-18 22:24:17 -0500

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+      <h1>Monsters Rolodex</h1>
         <SearchBox placeholder="search Monsters" handleSearch={this.handleSearch} />
         <CardList monsters={filterMonsters} />
       </div>
